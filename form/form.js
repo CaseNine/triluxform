69967d5eeeb72096689e07d93870f2456ff37a34
DannyVanIets <dannyvanbokhorst@live.nl>
2025-11-19 16:22:13 +0100

    The whole form is functional, but the design is lacking
    Removed some parts of the form I don't see back in the design

--- form/form.js
+++ form/form.js
@@ -15,7 +15,6 @@
   const projectName = document.getElementById('projectName');
   const jsonModalEl = document.getElementById('jsonModal');
   const jsonModal = new bootstrap.Modal(jsonModalEl);
-  var module_length = '0 mm';
 
   let idxCounter = 0;
 
@@ -27,17 +26,20 @@
       obj.room_name = b.querySelector('[name$="[room_name]"]')?.value || '';
       obj.length = parseFloat(b.querySelector('[name$="[length]"]')?.value || 0);
       obj.identical_lengths_lightlines = parseFloat(b.querySelector('[name$="[identical_lengths_lightlines]"]')?.value || 0);
-      obj.module_length = module_length;
-      obj.mounting_type = b.querySelector('[name$="[mounting_type]"]')?.value || '';
-      obj.mount_height = b.querySelector('[name$="[mount_height]"]')?.value || '';
+      module_length = b.querySelector('.module_length.active');
+      obj.module_length = module_length ? module_length.getAttribute('data-value') : '';
+      mount_height = b.querySelector('.mount_height.active');
+      obj.mount_height = mount_height ? mount_height.getAttribute('data-value') : '';
       const thumb = b.querySelector('.thumb.selected');
       obj.color = thumb ? thumb.getAttribute('data-value') : '';
       const opt = b.querySelector('.optbox.selected');
       obj.optic = opt ? opt.getAttribute('data-value') : '';
-      const dim = b.querySelector('[name$="[dimmable]"]:checked');
-      obj.dimmable = dim ? dim.value : 'no';
-      obj.inset_type = b.querySelector('[name$="[inset_type]"]')?.value || '';
-      obj.extra_mount = b.querySelector('[name$="[extra_mount]"]')?.value || '';
+      dimmable = b.querySelector('.dimmable.active');
+      obj.dimmable = dimmable ? dimmable.getAttribute('data-value') : '';
+      distribute_lightline = b.querySelector('.distribute_lightline.active');
+      obj.distribute_lightline = distribute_lightline ? distribute_lightline.getAttribute('data-value') : '';
+      montage = b.querySelector('.montage.active');
+      obj.montage = montage ? montage.getAttribute('data-value') : '';
       arr.push(obj);
     });
     return arr;
@@ -54,15 +56,15 @@
                       Lengte lichtlijn <div class="small-muted">${(c.length||0)} m </div>
                       <i>Aantal identieke lengtes lichtlijn</i> <div class="small-muted"> ${(c.identical_lengths_lightlines||0)} </div>
                       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M3 21V3h18v18zm2-2h14v-5H5zm0-7h14V5H5zm0 0V5z"/></svg>
-                      Module lengte <div class="small-muted">${(c.module_length)}</div>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 15 15"><path fill="currentColor" d="M14.6 4.01a.5.5 0 0 1 .4.49v6a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-6l.01-.1A.5.5 0 0 1 .5 4h14zM1 10h13V5h-1.075v1.5a.425.425 0 1 1-.85 0V5h-1.15v1.5a.425.425 0 1 1-.85 0V5h-1.15v2.5a.425.425 0 1 1-.85 0V5h-1.15v1.5a.425.425 0 1 1-.85 0V5h-1.15v1.5a.425.425 0 1 1-.85 0V5h-1.15v2.5a.425.425 0 1 1-.85 0V5H1z"/></svg>
+                      Module lengte <div class="small-muted">${(c.module_length||0)} mm</div>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16 7V2.6a.6.6 0 0 0-.6-.6H8.6a.6.6 0 0 0-.6.6v18.8a.6.6 0 0 0 .6.6h6.8a.6.6 0 0 0 .6-.6V17m0-10h-3m3 0v5m0 0h-3m3 0v5m0 0h-3"/></svg>
                       Montagehoogte/Lumenpakket <div class="small-muted">${(c.mount_height||0)}</div>
                       <i class="bi-sun"></i> Lichtkleur <div class="small-muted">${(c.color||0)}</div>
                       <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><circle cx="25.193" cy="24" r="10.574" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/><circle cx="21.331" cy="12.115" r="8.613" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/><circle cx="35.303" cy="16.655" r="8.613" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/><circle cx="35.303" cy="31.345" r="8.613" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/><circle cx="21.331" cy="35.885" r="8.613" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/><circle cx="12.697" cy="24" r="8.613" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/></svg>
                       Optiek <div class="small-muted">${c.optic || ''}</div>
-                      <i class="bi-sun"></i> Dimbaar <div class="small-muted">${c.dimmable === 'yes' ? 'Ja' : 'Nee'}</div>
-                      <i class="bi bi-grid-3x3-gap"></i> Indeling lichtlijn <div class="small-muted">${(c.color||0)}</div>
-                      <i class="bi bi-tools"></i> Montage <div class="small-muted">${(c.color||0)}</div></div>`;
+                      <i class="bi-sun"></i> Dimbaar <div class="small-muted">${(c.dimmable|| '<br>')}</div>
+                      <i class="bi bi-grid-3x3-gap"></i> Indeling lichtlijn <div class="small-muted">${(c.distribute_lightline||'<br>')}</div>
+                      <i class="bi bi-tools"></i> Montage <div class="small-muted">${(c.montage||'<br>')}</div></div>`;
       summaryList.appendChild(li);
     });
   }
@@ -111,14 +113,13 @@
         room_name: el.querySelector('[name$="[room_name]"]').value,
         length: el.querySelector('[name$="[length]"]').value,
         identical_lengths_lightlines: el.querySelector('[name$="[identical_lengths_lightlines]"]').value,
-        module_length: module_length,
-        mounting_type: el.querySelector('[name$="[mounting_type]"]').value,
-        mount_height: el.querySelector('[name$="[mount_height]"]').value,
+        module_length: (el.querySelector('.module_length.active')?.getAttribute('data-value')).value || '',
+        mount_height: (el.querySelector('.mount_height.active')?.getAttribute('data-value')).value || '',
         color: (el.querySelector('.thumb.selected')?.getAttribute('data-value')) || '',
         optic: (el.querySelector('.optbox.selected')?.getAttribute('data-value')) || '',
-        dimmable: el.querySelector('[name$="[dimmable]"]:checked')?.value || 'no',
-        inset_type: el.querySelector('[name$="[inset_type]"]').value,
-        extra_mount: el.querySelector('[name$="[extra_mount]"]').value
+        dimmable: (el.querySelector('.dimmable.active')?.getAttribute('data-value')).value || '',
+        distribute_lightline: (el.querySelector('.distribute_lightline.active')?.getAttribute('data-value')).value || '',
+        montage: (el.querySelector('.montage.active')?.getAttribute('data-value')).value || '',
       };
       const newEl = createConfig(data);
       // ensure it's visible
@@ -162,18 +163,17 @@
       if(initial.optic && o.getAttribute('data-value') === initial.optic) o.classList.add('selected');
     });
 
+    createBootstrapButtonClickEvents(el, 'module_length');
+    createBootstrapButtonClickEvents(el, 'mount_height');
+    createBootstrapButtonClickEvents(el, 'dimmable');
+    createBootstrapButtonClickEvents(el, 'distribute_lightline');
+    createBootstrapButtonClickEvents(el, 'montage');
+
     // set initial values if provided
     if(initial.room_name) el.querySelector('[name$="[room_name]"]').value = initial.room_name;
     if(initial.length) el.querySelector('[name$="[length]"]').value = initial.length;
-    //if(initial.module_length) el.querySelector('[name$="[module_length]"]').value = initial.module_length;
-    if(initial.mounting_type) el.querySelector('[name$="[mounting_type]"]').value = initial.mounting_type;
-    if(initial.mount_height) el.querySelector('[name$="[mount_height]"]').value = initial.mount_height;
-    if(initial.dimmable){
-      const rad = el.querySelector(`[name$="[dimmable]"][value="${initial.dimmable}"]`);
-      if(rad) rad.checked = true;
-    }
-    if(initial.inset_type) el.querySelector('[name$="[inset_type]"]').value = initial.inset_type;
-    if(initial.extra_mount) el.querySelector('[name$="[extra_mount]"]').value = initial.extra_mount;
+    //if(initial.inset_type) el.querySelector('[name$="[inset_type]"]').value = initial.inset_type;
+    //if(initial.extra_mount) el.querySelector('[name$="[extra_mount]"]').value = initial.extra_mount;
 
     configsContainer.appendChild(el);
 
@@ -199,31 +199,6 @@
       updateSummary();
     });
 
-    const twotwofivezero = el.querySelector('[name$="[twotwofivezero]"');
-    const onefivezerozero = el.querySelector('[name$="[onefivezerozero]"');
-
-    twotwofivezero.addEventListener('click', () => {
-      if(twotwofivezero.getAttribute("aria-pressed") == 'true'){ // Click on the button before it's active.
-        onefivezerozero.classList.remove('active');
-        onefivezerozero.setAttribute("aria-pressed", "false");
-        module_length = '2.250 mm';
-      } else { // Click on the button to set it inactive.
-        module_length = '0 mm';
-      }
-      updateSummary();
-    });
-
-    onefivezerozero.addEventListener('click', () => {
-      if(onefivezerozero.getAttribute("aria-pressed") == 'true'){ // Click on the button before it's active.
-        twotwofivezero.classList.remove('active');
-        twotwofivezero.setAttribute("aria-pressed", "false");
-        module_length = '1.500 mm';
-      } else { // Click on the button to set it inactive.
-        module_length = '0 mm';
-      }
-      updateSummary();
-    });
-
     updateSummary();
     return appended;
   }
@@ -244,14 +219,13 @@
         room_name: last.querySelector('[name$="[room_name]"]').value,
         length: last.querySelector('[name$="[length]"]').value,
         identical_lengths_lightlines: last.querySelector('[name$="[identical_lengths_lightlines]"]').value,
-        module_length: last.querySelector('[name$="[module_length]"]').value,
-        mounting_type: last.querySelector('[name$="[mounting_type]"]').value,
-        mount_height: last.querySelector('[name$="[mount_height]"]').value,
+        module_length: (last.querySelector('.module_length.active')?.getAttribute('data-value')) || '',
+        mount_height: (last.querySelector('.mount_height.active')?.getAttribute('data-value')) || '',
         color: (last.querySelector('.thumb.selected')?.getAttribute('data-value')) || '',
         optic: (last.querySelector('.optbox.selected')?.getAttribute('data-value')) || '',
-        dimmable: last.querySelector('[name$="[dimmable]"]:checked')?.value || 'no',
-        inset_type: last.querySelector('[name$="[inset_type]"]').value,
-        extra_mount: last.querySelector('[name$="[extra_mount]"]').value
+        dimmable: (last.querySelector('.dimmable.active')?.getAttribute('data-value')) || '',
+        distribute_lightline: (last.querySelector('.distribute_lightline.active')?.getAttribute('data-value')) || '',
+        montage: (last.querySelector('.montage.active')?.getAttribute('data-value')) || '',
       };
       createConfig(data);
     } else {
@@ -274,8 +248,8 @@
   });
 
   // initial seeds
-  createConfig({ room_name: 'Meeting Room 01', length: '8.00', module_length: '2.25', mounting_type: 'inbouw', mount_height: '80', color: '3000K', optic: 'wide', dimmable: 'no' });
-  createConfig({ room_name: 'Kantoor Tuin', length: '14.20', module_length: '3.0', mounting_type: 'pendel', mount_height: '2300', color: '4000K', optic: 'narrow', dimmable: 'yes' });
+  createConfig({ room_name: 'Meeting Room 01', length: '8.00', module_length: '2.25', mount_height: '80', color: '3000K', optic: 'wide', dimmable: 'no' });
+  createConfig({ room_name: 'Kantoor Tuin', length: '14.20', module_length: '3.0', mount_height: '2300', color: '4000K', optic: 'narrow', dimmable: 'yes' });
   updateSummary();
 
   finishButtons.forEach(b => b.addEventListener('click', () => {
@@ -290,4 +264,17 @@
     console.log('Payload:', payload);
   });
 
+  function createBootstrapButtonClickEvents(el, className){
+    el.querySelectorAll('.' + className).forEach(o => {
+      o.addEventListener('click', () => {
+        el.querySelectorAll('.' + className + '.active').forEach(x => {
+          if(x != o){
+            x.classList.remove('active');
+            x.setAttribute("aria-pressed", "false");
+          }
+        });
+        updateSummary();
+      });
+    });
+  }
 })();
